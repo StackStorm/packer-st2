dcc2eda22a6d666fb04b55017907f9b0bfc44034
armab <dev@arma.biz>
2018-04-12 23:40:36 +0100

    Add pre_cleanup & post_cleanup scripts

---
+++ scripts/pre_cleanup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -eux
+###
+## Cleanup scripts executed before installing StackStorm
+###
+
+# Delete all Linux headers
+dpkg --list \
+  | awk '{ print $2 }' \
+  | grep 'linux-headers' \
+  | xargs apt-get -y purge;
+
+# Remove specific Linux kernels, such as linux-image-3.11.0-15-generic but
+# keeps the current kernel and does not touch the virtual packages,
+# e.g. 'linux-image-generic', etc.
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep 'linux-image-.*-generic' \
+    | grep -v `uname -r` \
+    | xargs apt-get -y purge;
+
+# Delete Linux source
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep linux-source \
+    | xargs apt-get -y purge;
+
+# Delete development packages
+# NB! Run this before installing StackStorm as it may pull some 'dev' dependencies itself
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep -- '-dev$' \
+    | xargs apt-get -y purge;
+
+# delete docs packages
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep -- '-doc$' \
+    | xargs apt-get -y purge;
+
+# Delete X11 libraries
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
+
+# Delete obsolete networking
+apt-get -y purge ppp pppconfig pppoeconf;
+
+# Delete oddities
+apt-get -y purge popularity-contest installation-report command-not-found command-not-found-data friendly-recovery fonts-ubuntu-font-family-console laptop-detect;
+
+# Delete the massive firmware packages
+apt-get -y purge linux-firmware
