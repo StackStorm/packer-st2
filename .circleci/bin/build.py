95e5d7f91b6da6f55258b3b777be988e7824d96b
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-05-03 16:01:35 -0700

    Remove license_key parameter

--- .circleci/bin/build.py
+++ .circleci/bin/build.py
@@ -2,11 +2,10 @@ from fabric.api import run
 from fabric.context_managers import cd
 
 
-def build(license_key):
+def build():
     """
     Run the OVA build on remote host with Fabric.
 
-    :param license_key: Required StackStorm Enterprise license key.
     :return:
     """
     with cd('/home/ova'):
