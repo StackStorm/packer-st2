b991d8d4d31f1cb0a9e79cf1673b5b4a2ff2b7bc
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-05-04 12:47:04 -0700

    Remove unused scripts/reboot.sh

--- scripts/reboot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eu                                                                                                          
-
-echo -e '\033[33mRebooting VM before running Integration Tests ...\033[0m'
-reboot
