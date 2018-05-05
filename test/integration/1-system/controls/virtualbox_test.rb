2e1aa9ec2c18cd5b0adbd0d8a59f269b0cbc3e00
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-05-04 17:44:45 -0700

    Iterate over KERNEL_MODULES to de-duplicate code

--- test/integration/1-system/controls/virtualbox_test.rb
+++ test/integration/1-system/controls/virtualbox_test.rb
@@ -2,6 +2,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
+KERNEL_MODULES = %w(
+  vboxvideo vboxsf vboxguest
+).freeze
+
 control 'vbox-guest-additions' do
   title 'Verify Virtualbox guest additions'
   desc '
@@ -10,22 +14,12 @@ control 'vbox-guest-additions' do
       https://www.virtualbox.org/manual/ch04.html#idm1873
   '
 
-  describe kernel_module('vboxvideo') do
-    it { should be_loaded }
-    it { should_not be_disabled }
-    it { should_not be_blacklisted }
-  end
-
-  describe kernel_module('vboxsf') do
-    it { should be_loaded }
-    it { should_not be_disabled }
-    it { should_not be_blacklisted }
-  end
-
-  describe kernel_module('vboxguest') do
-    it { should be_loaded }
-    it { should_not be_disabled }
-    it { should_not be_blacklisted }
+  KERNEL_MODULES.each do |module_name|
+    describe kernel_module(module_name) do
+      it { should be_loaded }
+      it { should_not be_disabled }
+      it { should_not be_blacklisted }
+    end
   end
 
   describe processes('VBoxService') do
