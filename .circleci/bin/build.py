9566ad0760de2fdb60511f85f2d5288a2cf7de79
armab <dev@arma.biz>
2018-05-14 12:56:31 +0100

    Pass 'ST2_VERSION' and 'BOX_VERSION' env vars to Fabric

--- .circleci/bin/build.py
+++ .circleci/bin/build.py
@@ -1,12 +1,17 @@
+import os
 from fabric.api import run
-from fabric.context_managers import cd
+from fabric.context_managers import cd, shell_env
 
 
 def build():
     """
-    Run the OVA build on remote host with Fabric.
+    Run the Packer build on remote host with Fabric.
+
+    Bypass 'ST2_VERSION' and 'BOX_VERSION' ENV vars.
 
     :return:
     """
     with cd('/home/ova'):
-        run('make build')
+        with shell_env(ST2_VERSION=os.environ.get('ST2_VERSION'),
+                       BOX_VERSION=os.environ.get('BOX_VERSION')):
+            run('make build')
